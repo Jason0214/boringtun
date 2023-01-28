c483cb9a952ac73148e8da697f1200934859b1bd
Jason0214 <lujc.me@gmail.com>
2023-01-28 22:42:48 +0800

    Fix test and ensure the no obfuscator version is working

--- boringtun/src/device/integration_tests/mod.rs
+++ boringtun/src/device/integration_tests/mod.rs
@@ -5,7 +5,9 @@
 // Those tests require docker and sudo privileges to run
 #[cfg(all(test))]
 mod tests {
+    use crate::device::udp::UDPSocket;
     use crate::device::{DeviceConfig, DeviceHandle};
+    use crate::device::BoringTunDevice;
     use base64::encode as base64encode;
     use hex::encode;
     use rand_core::OsRng;
@@ -58,7 +60,7 @@ mod tests {
 
     /// Represents a single WireGuard interface on local machine
     struct WGHandle {
-        _device: DeviceHandle,
+        _device: Box<dyn BoringTunDevice>,
         name: String,
         addr_v4: IpAddr,
         addr_v6: IpAddr,
@@ -278,7 +280,7 @@ mod tests {
         fn init_with_config(addr_v4: IpAddr, addr_v6: IpAddr, config: DeviceConfig) -> WGHandle {
             // Generate a new name, utun100+ should work on macOS and Linux
             let name = format!("utun{}", NEXT_IFACE_IDX.fetch_add(1, Ordering::Relaxed));
-            let _device = DeviceHandle::new(&name, config).unwrap();
+            let _device = DeviceHandle::<UDPSocket>::new(&name, config).unwrap();
             WGHandle {
                 _device,
                 name,
